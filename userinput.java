5e14d9974017d38e1a1183a4aedfaf63771f5558
Anders <anders.ym@hotmail.no>
2025-07-06 12:07:16 +0200

    userinput_ferdig

--- userinput.java
+++ userinput.java
@@ -24,6 +24,46 @@ public class userinput {
         System.out.println("Your gpa is: " + gpa);
         System.out.println("Student: " + isStudent);
 
+        if(isStudent){
+            System.out.println("You are enrolled as a student");
+        }
+        else{
+            System.out.println("You are NOT enrolled");
+        }
+
+        //Problemer:
+
+        System.out.println("Enter your age: ");
+        int alder = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter your favorite color");
+        String color = scanner.nextLine();
+
+        System.out.println("You are " + alder + " years old");
+        System.out.println("You like the color " + color);
+
+
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+    
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble(); 
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble(); 
+        
+        area = width * height;
+
+        System.out.println("The erea is: " + area + "cm²");
+
+        
+
+
         scanner.close();
 
     }
