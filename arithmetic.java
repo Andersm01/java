4dfeb35b774779dd014f8646e9b0f640ea5902d4
Anders <anders.ym@hotmail.no>
2025-07-06 19:13:57 +0200

    arithmtic

---
+++ arithmetic.java
@@ -0,0 +1,50 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        
+
+
+        int x = 10;
+        int y = 3;
+        int z;
+
+        z = x + y;
+        z = x - y;
+        z = x - y;
+        z = x - y;
+        z = x / y;
+        z = x % y;
+
+        System.out.println(z);
+
+
+
+        
+
+        //x = x + y;
+        //x += y;
+        //x = x - y;
+        //x -= y;
+        //x = x * y;
+        //x *= y;
+        //x = x / y; //gjør double for desimal
+        //x = x % y;
+        x %= y;
+    
+
+        System.out.println(x);
+
+
+        x++;
+        x--;
+        x--;
+        x--;
+
+        System.out.println(x);
+
+        double result = 3 + 4 * (7 - 5) / 2.0; //P-E-M-D-A-S
+
+        System.out.println(result);
+
+
+    }
+}
